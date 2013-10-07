d647b13a0c5b374fc127341f3457b8da88959f0a
Tristan <klempka.tristan@gmail.com>
2013-10-07 13:12:56 +0200

    Helper function to rotate entities

--- Player_Prototype/Source/Entity.cpp
+++ Player_Prototype/Source/Entity.cpp
@@ -1,5 +1,5 @@
 #include "Entity.h"
-
+#define _USE_MATH_DEFINES
 const float SCALE = 30.f; // Box2D works in a scale of 30 pixels = 1 meter
 
 Entity::Entity():
@@ -24,4 +24,9 @@ void Entity::setPosition(float x, float y)
     m_body->SetTransform(b2Vec2(x/SCALE, y/SCALE), m_body->GetAngle());
 }
 
+void Entity::setRotation(float angle)
+{
+    sf::Transformable::setRotation(angle);
+    m_body->SetTransform(b2Vec2(sf::Transformable::getPosition().x/SCALE, sf::Transformable::getPosition().y/SCALE), (angle*pi())/180);
+}
 // TODO set rotation
