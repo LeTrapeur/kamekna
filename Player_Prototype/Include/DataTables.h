c7128979e78419ed66ede327a4b1cb2a3d50f2b0
Tristan Klempka <klempka.tristan@gmail.com>
2013-10-09 13:54:14 +0200

    started to move the body creation to class entity

--- Player_Prototype/Include/DataTables.h
+++ Player_Prototype/Include/DataTables.h
@@ -1,14 +1,17 @@
 #ifndef DATATABLES_H
 #define DATATABLES_H
 
-// FOR EXAMPLE
+#include <Box2D/Box2D.h>
+
+#include "Entity.h"
+
 
 struct EntityData
 {
-//    int width;
-//    int height;
-//    b2BodyDef bodyDef;
-//    std::vector<b2FixtureDef> fixturesDef;
+    int width;
+    int height;
+    b2BodyDef bodyDef;
+    std::vector<b2FixtureDef> fixturesDef;
 };
 
 struct HeroData
@@ -22,5 +25,6 @@ struct PlatformData
 
 };
 
+std::vector<EntityData> initializeEntityData();
 
 #endif // DATATABLES_H
--- Player_Prototype/Source/Hero.cpp
+++ Player_Prototype/Source/Hero.cpp
@@ -3,40 +3,16 @@
 const float SCALE = 30.f; // Box2D works in a scale of 30 pixels = 1 meter
 extern int numFootContacts;
 
-Hero::Hero(b2World& world)
+Hero::Hero(b2World& world): Entity(world, Entity::Type::Hero)
 {
     // Player
     const int PLAYER_WIDTH = 32;
     const int PLAYER_HEIGHT = 64;
 
-//    setPosition(1280/2, 720/2);// Center
+    setPosition(1280/2, 720/2);// Center
     setOrigin(sf::Vector2f(PLAYER_WIDTH/2,PLAYER_HEIGHT/2));
     m_shape.setSize(sf::Vector2f(PLAYER_WIDTH,PLAYER_HEIGHT));
     m_shape.setFillColor(sf::Color::Red);
-
-    b2BodyDef PlayerBodyDef;
-    PlayerBodyDef.type = b2_dynamicBody;
-    PlayerBodyDef.position = b2Vec2(1280/2/SCALE, 720/2/SCALE);
-    PlayerBodyDef.fixedRotation = true;
-    m_body = world.CreateBody(&PlayerBodyDef);
-
-    b2FixtureDef PlayerFixtureDef;
-    b2PolygonShape PlayerShape;
-    PlayerShape.SetAsBox((PLAYER_WIDTH/2.0f)/SCALE, (PLAYER_HEIGHT/2.0f)/SCALE);
-    PlayerFixtureDef.shape = &PlayerShape;
-    PlayerFixtureDef.density = 1.0f;
-    PlayerFixtureDef.friction = 0.3f;
-    m_body->CreateFixture(&PlayerFixtureDef);
-
-    //add foot sensor fixture
-    b2PolygonShape FootShape;
-    FootShape.SetAsBox(((PLAYER_WIDTH-10)/2.0f)/SCALE, 5/SCALE, b2Vec2(0,(PLAYER_HEIGHT/2.0f)/SCALE), 0);
-    b2FixtureDef footSensorFixture;
-    footSensorFixture.shape = &FootShape;
-    footSensorFixture.isSensor = true;
-    footSensorFixture.userData = ((void*)3);
-    m_body->CreateFixture(&footSensorFixture);
-
 }
 
 void Hero::handleEvent(const sf::Event& event)
