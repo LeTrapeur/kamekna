8646d47ee78f00e42dd4988b95dab2ec622cf245
Tristan Klempka <klempka.tristan@gmail.com>
2013-10-09 14:44:56 +0200

    Separate creation of body from entity derived class
    add data tables

--- Player_Prototype/Source/DataTables.cpp
+++ Player_Prototype/Source/DataTables.cpp
@@ -2,6 +2,8 @@
 
 const float SCALE = 30.f; // Box2D works in a scale of 30 pixels = 1 meter
 
+
+// todo enlever les static (vars globales)
 std::vector<EntityData> initializeEntityData()
 {
     std::vector<EntityData> data(Entity::TypeCount);
@@ -16,20 +18,35 @@ std::vector<EntityData> initializeEntityData()
     data[Entity::Hero].bodyDef = PlayerBodyDef;
 
     b2FixtureDef PlayerFixtureDef;
-    b2PolygonShape PlayerShape;
+    static b2PolygonShape PlayerShape;
     PlayerShape.SetAsBox((32/2.0f)/SCALE, (64/2.0f)/SCALE);
     PlayerFixtureDef.shape = &PlayerShape;
     PlayerFixtureDef.density = 1.0f;
     PlayerFixtureDef.friction = 0.3f;
     data[Entity::Hero].fixturesDef.push_back(PlayerFixtureDef);
 
-    b2PolygonShape FootShape;
+    b2FixtureDef footSensorFixtureDef;
+    static b2PolygonShape FootShape;
     FootShape.SetAsBox(((32-10)/2.0f)/SCALE, 5/SCALE, b2Vec2(0,(64/2.0f)/SCALE), 0);
-    b2FixtureDef footSensorFixture;
-    footSensorFixture.shape = &FootShape;
-    footSensorFixture.isSensor = true;
-    footSensorFixture.userData = ((void*)3);
-    data[Entity::Hero].fixturesDef.push_back(footSensorFixture);
+    footSensorFixtureDef.shape = &FootShape;
+    footSensorFixtureDef.isSensor = true;
+    footSensorFixtureDef.userData = ((void*)3);
+    data[Entity::Hero].fixturesDef.push_back(footSensorFixtureDef);
+
+    data[Entity::Platform].width = 1024;
+    data[Entity::Platform].height = 32;
+
+    b2BodyDef GroundBodyDef;
+    GroundBodyDef.position = b2Vec2((1280/2)/SCALE, (720-(32/2))/SCALE);
+    GroundBodyDef.type = b2_staticBody;
+    data[Entity::Platform].bodyDef = GroundBodyDef;
+
+    b2FixtureDef GroundFixtureDef;
+    static b2PolygonShape GroundShape;
+    GroundShape.SetAsBox((1024/2.0f)/SCALE, (32/2.0f)/SCALE);
+    GroundFixtureDef.shape = &GroundShape;
+    GroundFixtureDef.density = 1.0f;
+    data[Entity::Platform].fixturesDef.push_back(GroundFixtureDef);
 
     return data;
 }
--- Player_Prototype/Source/Entity.cpp
+++ Player_Prototype/Source/Entity.cpp
@@ -10,55 +10,10 @@ namespace
 Entity::Entity(b2World& world, Entity::Type type):
     m_body(nullptr)
 {
-    if(type == Entity::Hero)
+    m_body = world.CreateBody(&dataTable[type].bodyDef);
+    for(auto it = dataTable[type].fixturesDef.begin(); it != dataTable[type].fixturesDef.end(); it++)
     {
-        const int PLAYER_WIDTH = 32;
-        const int PLAYER_HEIGHT = 64;
-//        b2BodyDef PlayerBodyDef;
-//        PlayerBodyDef.type = b2_dynamicBody;
-//        PlayerBodyDef.position = b2Vec2(1280/2/SCALE, 720/2/SCALE);
-//        PlayerBodyDef.fixedRotation = true;
-        m_body = world.CreateBody(&dataTable[Entity::Hero].bodyDef);
-//
-//        for(auto it = dataTable[Entity::Hero].fixturesDef.begin(); it != dataTable[Entity::Hero].fixturesDef.end(); it++)
-//        {
-//            b2FixtureDef tempFixtureDef;
-//            tempFixtureDef = (*it);
-//            m_body->CreateFixture(&tempFixtureDef);
-//        }
-
-        b2FixtureDef PlayerFixtureDef;
-        b2PolygonShape PlayerShape;
-        PlayerShape.SetAsBox((PLAYER_WIDTH/2.0f)/SCALE, (PLAYER_HEIGHT/2.0f)/SCALE);
-        PlayerFixtureDef.shape = &PlayerShape;
-        PlayerFixtureDef.density = 1.0f;
-        PlayerFixtureDef.friction = 0.3f;
-        m_body->CreateFixture(&PlayerFixtureDef);
-
-        //add foot sensor fixture
-        b2PolygonShape FootShape;
-        FootShape.SetAsBox(((PLAYER_WIDTH-10)/2.0f)/SCALE, 5/SCALE, b2Vec2(0,(PLAYER_HEIGHT/2.0f)/SCALE), 0);
-        b2FixtureDef footSensorFixture;
-        footSensorFixture.shape = &FootShape;
-        footSensorFixture.isSensor = true;
-        footSensorFixture.userData = ((void*)3);
-        m_body->CreateFixture(&footSensorFixture);
-    }
-    else if (type == Entity::Platform)
-    {
-        const int FLOOR_WIDTH = 1024;
-        const int FLOOR_HEIGHT = 32;
-        b2BodyDef GroundBodyDef;
-        GroundBodyDef.position = b2Vec2((1280/2)/SCALE, (720-(FLOOR_HEIGHT/2))/SCALE);
-        GroundBodyDef.type = b2_staticBody;
-        m_body = world.CreateBody(&GroundBodyDef);
-
-        b2FixtureDef GroundFixtureDef;
-        b2PolygonShape GroundShape;
-        GroundShape.SetAsBox((FLOOR_WIDTH/2.0f)/SCALE, (FLOOR_HEIGHT/2.0f)/SCALE);
-        GroundFixtureDef.shape = &GroundShape;
-        GroundFixtureDef.density = 1.0f;
-        m_body->CreateFixture(&GroundFixtureDef);
+        m_body->CreateFixture(&(*it));
     }
 }
 
