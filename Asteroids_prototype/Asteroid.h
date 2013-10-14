4faf4079ded87d5fd9a2c4390f2075b6a621fcab
Tristan <klempka.tristan@gmail.com>
2013-10-14 16:04:31 +0200

    Outsourced the making process of random asteroid

--- Asteroids_prototype/Asteroid.h
+++ Asteroids_prototype/Asteroid.h
@@ -1,13 +1,16 @@
 #ifndef ASTEROID_H
 #define ASTEROID_H
 
-#include<vector>
+#include <vector>
 #include <SFML/graphics.hpp>
+
+#include "Utility.h"
 
 class Asteroid : public sf::Transformable, public sf::Drawable, private sf::NonCopyable
 {
     public:
         Asteroid();
+        friend void AsteroidGenerator::makeRandomAsteroid(Asteroid* asteroid);
 
     private:
         virtual void draw (sf::RenderTarget& target, sf::RenderStates states) const;
--- Asteroids_prototype/Utility.cpp
+++ Asteroids_prototype/Utility.cpp
@@ -1,1 +1,79 @@
 #include "Utility.h"
+#include "Asteroid.h"
+
+
+namespace AsteroidGenerator
+{
+    void makeRandomAsteroid(Asteroid* asteroid)
+    {
+        std::vector< sf::Vector2f > randomPoints;
+        randomPoints.push_back(sf::Vector2f(100,500));
+        randomPoints.push_back(sf::Vector2f(120,375));
+        randomPoints.push_back(sf::Vector2f(400,300));
+        randomPoints.push_back(sf::Vector2f(50,500));
+        randomPoints.push_back(sf::Vector2f(70,220));
+
+        std::vector< sf::Vector2f > sortPoints;
+        sortPoints = Utility::ConvexHull(randomPoints);
+
+        asteroid->shape.setPointCount(sortPoints.size());
+        asteroid->shape.setFillColor(sf::Color(80, 170, 200, 200));
+        for(size_t i=0; i< sortPoints.size(); i++)
+        {
+            asteroid->shape.setPoint(i, sortPoints.at(i));
+        }
+    }
+}
+
+namespace Utility
+{
+    int Orientation(const sf::Vector2f& p1, const sf::Vector2f& p2, const sf::Vector2f& p)
+    {
+        // Determinant
+        int Orin = (p2.x - p1.x) * (p.y - p1.y) - (p.x - p1.x) * (p2.y - p1.y);
+
+        if (Orin > 0)
+            return -1; //          (* Orientaion is to the left-hand side  *)
+        if (Orin < 0)
+            return 1; // (* Orientaion is to the right-hand side *)
+
+        return 0; //  (* Orientaion is neutral aka collinear  *)
+    }
+
+    sf::Vector2f getMostLeft(const std::vector< sf::Vector2f >& pts)
+    {
+        sf::Vector2f ptLeft = pts.at(0);
+        for(size_t i = 1; i < pts.size() ; i++)
+            {
+                if(pts.at(i).x < ptLeft.x)
+                    ptLeft = pts[i];
+            }
+        return ptLeft;
+    }
+
+    std::vector< sf::Vector2f > ConvexHull(const std::vector< sf::Vector2f >& points)
+            {
+                std::vector< sf::Vector2f > hull;
+                // get leftmost point
+                sf::Vector2f vPointOnHull = getMostLeft(points);
+                sf::Vector2f vEndpoint;
+                do
+                {
+                    hull.push_back(vPointOnHull);
+                    vEndpoint = points.at(0);
+
+                    for (size_t i = 1; i < points.size(); i++)
+                    {
+                        if ((vPointOnHull == vEndpoint)
+                            || (Orientation(vPointOnHull, vEndpoint, points[i]) == -1))
+                        {
+                            vEndpoint = points[i];
+                        }
+                    }
+                    vPointOnHull = vEndpoint;
+                }
+                while (vEndpoint != hull.at(0));
+
+                return hull;
+    }
+}
