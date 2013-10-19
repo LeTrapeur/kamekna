e9c50bd82717bc47a1daf5a9b513a709c182a92b
Tristan <klempka.tristan@gmail.com>
2013-10-19 19:26:39 +0200

    Update transform but setposition still bug

--- Asteroids_prototype/Asteroid.cpp
+++ Asteroids_prototype/Asteroid.cpp
@@ -3,20 +3,21 @@
 Asteroid::Asteroid()
 {
 
-    textureAsteroid.loadFromFile("texture.png");
+    textureAsteroid.loadFromFile("textureAsteroid.png");
     shape.setTexture(&textureAsteroid);
 
 }
 
 void Asteroid::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-    target.draw(shape);
+    states.transform *= getTransform();
+    target.draw(shape, states);
     for(size_t i = 0; i < shape.getPointCount(); i++)
     {
         sf::RectangleShape point(sf::Vector2f(5.f, 5.f));
         point.setFillColor(sf::Color::Red);
         point.setOrigin(sf::Vector2f(2.5f, 2.5f));
         point.setPosition(shape.getPoint(i));
-        target.draw(point);
+        target.draw(point, states);
     }
 }
