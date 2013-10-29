5ba0f1a9feda2f7cd5d57c92fd213b9c57c2cb6b
Tristan Klempka <klempka.tristan@gmail.com>
2013-10-29 20:47:27 +0100

    minor corrections

--- Player_Prototype/Include/ResourceHolder.h
+++ Player_Prototype/Include/ResourceHolder.h
@@ -9,34 +9,34 @@
 
 namespace sf
 {
-        class Texture;
+    class Texture;
 }
 
 namespace Textures
 {
-        enum ID
-        {
-            SpaceBackground
-        };
+    enum ID
+    {
+        SpaceBackground
+    };
 }
 
 template <typename Resource, typename Identifier>
 class ResourceHolder
 {
-        public:
-                void load(Identifier id, const std::string& filename);
+    public:
+        void load(Identifier id, const std::string& filename);
 
-                template <typename Parameter>
-                void load(Identifier id, const std::string& filename, const Parameter& secondParam);
+        template <typename Parameter>
+        void load(Identifier id, const std::string& filename, const Parameter& secondParam);
 
-                Resource& get(Identifier id);
-                const Resource& get(Identifier id) const;
+        Resource& get(Identifier id);
+        const Resource& get(Identifier id) const;
 
-        private:
-                void insertResource(Identifier id, std::unique_ptr<Resource> resource);
+    private:
+        void insertResource(Identifier id, std::unique_ptr<Resource> resource);
 
-        private:
-                std::map<Identifier, std::unique_ptr<Resource>>        mResourceMap;
+    private:
+        std::map<Identifier, std::unique_ptr<Resource>> mResourceMap;
 };
 
 typedef ResourceHolder<sf::Texture, Textures::ID> TextureHolder;
