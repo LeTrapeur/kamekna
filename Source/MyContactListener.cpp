c22726004480b80c499f5eff412c592b6e3abdfd
Tristan Klempka <klempka.tristan@gmail.com>
2014-01-09 17:58:41 +0100

    contactlistener clean

--- Source/MyContactListener.cpp
+++ Source/MyContactListener.cpp
@@ -19,6 +19,8 @@ void MyContactListener::BeginContact(b2Contact* contact)
         auto& enemy = static_cast<Actor&>(*entities.first);
         auto& projectile = static_cast<Projectile&>(*entities.second);
         enemy.takeDamage(projectile.getDamage());
+
+        // TODO projectile.destroy();
     }
 
     if(matchesCategory(entities, Category::PlayerActor, Category::LowerScene))
@@ -26,25 +28,6 @@ void MyContactListener::BeginContact(b2Contact* contact)
         auto& player = static_cast<Actor&>(*entities.first);
         player.addFootContact();
     }
-
-//    if(A->getCategory() == Category::EnemyActor && B->getCategory() == Category::AlliedProjectile)
-//    {
-//        Actor* actor = dynamic_cast<Actor*>(A);
-//        Projectile* projectile = dynamic_cast<Projectile*>(B);
-//        if(actor && projectile)
-//            actor->takeDamage(5);
-//    }
-//    {
-//        actor->addFootContact();
-//        return;
-//    }
-//    actor = dynamic_cast<Actor*>(B);
-//    if (actor)
-//    {
-//        actor->addFootContact();
-//        return
-//    }
-
 }
 
 void MyContactListener::EndContact(b2Contact* contact)
@@ -58,21 +41,6 @@ void MyContactListener::EndContact(b2Contact* contact)
         auto& player = static_cast<Actor&>(*entities.first);
         player.removeFootContact();
     }
-
-
-
-//    Entity* A = static_cast<Entity*>(contact->GetFixtureA()->GetUserData());
-//    Entity* B = static_cast<Entity*>(contact->GetFixtureB()->GetUserData());
-//    Actor* actor = dynamic_cast<Actor*>(A);
-//    if (actor)
-//    {
-//        actor->removeFootContact();
-//    }
-//    actor = dynamic_cast<Actor*>(B);
-//    if (actor)
-//    {
-//        actor->removeFootContact();
-//    }
 }
 
 bool MyContactListener::matchesCategory(std::pair<Entity*,Entity*>& entities, Category::Type type1, Category::Type type2)
